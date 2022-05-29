9cb3e723920dcf8854c62ecb289e3fd42868eb06
cesarcor <cesarcorrechel@gmail.com>
2022-05-28 22:22:38 -0600

    initial commit

---
+++ front-page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+	<section class="site-section">
+        <div class="hero bg-primary flex justify-center items-center h-screen">
+            <div class="hero-body object-center">
+                <div class="hero-text text-center">
+                    <div class="hero-text__big text-white">
+                        <h1 class="text-3xl">We create digital products to solve your problems</h1>
+                    </div>
+                    <div class="hero-text__small text-gray">
+                        <p>A fully integrated digital factory that will help you achieve your goals in your company</p>
+                    </div>
+                </div>
+                <div class="hero-buttons">
+                    <a href="#" class="bg-white text-primary">Get Started --></a>
+                    <a href="#">Learn More</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <?php
+get_footer();
