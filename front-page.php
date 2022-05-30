e167cdf0563835c97d8de404a9b5e116d94c3e57
cesarcor <cesarcorrechel@gmail.com>
2022-05-29 20:41:23 -0600

    add hero section

--- front-page.php
+++ front-page.php
@@ -2,20 +2,19 @@
 
 	<section class="site-section">
         <div class="hero bg-primary flex justify-center items-center h-screen">
-            <div class="hero-body object-center">
-                <div class="hero-text text-center">
-                    <div class="hero-text__big text-white">
-                        <h1 class="text-3xl">We create digital products to solve your problems</h1>
+            <div class="hero-container container object-center">
+                <div class="hero-text text-center mb-10">
+                    <div class="hero-text__big text-white mb-5">
+                        <h1 class="text-heading font-bold">We create digital products <br/> and solve your problems</h1>
                     </div>
                     <div class="hero-text__small text-gray">
-                        <p>A fully integrated digital factory that will help you achieve your goals in your company</p>
+                        <p class="font-normal text-regular">A fully integrated digital factory that will help you <br/> achieve your goals in your company</p>
                     </div>
                 </div>
-                <div class="hero-buttons">
-                    <a href="#" class="bg-white text-primary">Get Started --></a>
-                    <a href="#">Learn More</a>
+                <div class="flex justify-center hero-buttons">
+                    <a href="#" class="btn bg-white text-primary rounded-full font-medium hover:bg-terciary">Get Started --></a>
+                    <a href="#" class="btn text-white hover:text-terciary">Learn More</a>
                 </div>
-
             </div>
         </div>
     </section>
