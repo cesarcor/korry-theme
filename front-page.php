9385175924d6bf7b4b6742fec7891711cf85160c
cesarcor <cesarcorrechel@gmail.com>
2022-05-30 21:25:17 -0600

    add video/stats section

--- front-page.php
+++ front-page.php
@@ -1,23 +1,56 @@
 <?php get_header(); ?>
 
-	<section class="site-section">
-        <div class="hero bg-primary flex justify-center items-center h-screen">
-            <div class="hero-container container object-center">
-                <div class="hero-text text-center mb-10">
-                    <div class="hero-text__big text-white mb-5">
-                        <h1 class="text-heading font-bold">We create digital products <br/> and solve your problems</h1>
-                    </div>
-                    <div class="hero-text__small text-gray">
-                        <p class="font-normal text-regular">A fully integrated digital factory that will help you <br/> achieve your goals in your company</p>
-                    </div>
+<section class="site-section">
+    <div class="hero bg-primary flex justify-center items-center h-screen">
+        <div class="hero-container container object-center">
+            <div class="hero-text text-center mb-10">
+                <div class="hero-text__big text-white mb-5">
+                    <h1 class="text-heading font-bold">We create digital products <br /> and solve your problems</h1>
                 </div>
-                <div class="flex justify-center hero-buttons">
-                    <a href="#" class="btn bg-white text-primary rounded-full font-medium hover:bg-terciary">Get Started --></a>
-                    <a href="#" class="btn text-white hover:text-terciary">Learn More</a>
+                <div class="hero-text__small text-gray">
+                    <p class="font-normal text-regular">A fully integrated digital factory that will help you <br />
+                        achieve your goals in your company</p>
+                </div>
+            </div>
+            <div class="flex justify-center hero-buttons">
+                <a href="#" class="btn bg-white text-primary rounded-full font-medium hover:bg-terciary">Get Started
+                    --></a>
+                <a href="#" class="btn text-white hover:text-terciary">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="site-section">
+    <div class="container mx-auto">
+        <div class="homepage-video-container relative">
+            <iframe class="homepage-video" src="https://www.youtube.com/embed/zwUsFN__jtE" title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        </div>
+        <div>
+            <div class="flex flex-wrap md:flex-nowrap justify-between text-center">
+                <div class="mb-4 min-w-[50%] md:min-w-[25%]">
+                    <span class="font-bold text-xxxl">2.5K+</span>
+                    <span class="text-regular text-gray block">Jobs done successfully</span>
+                </div>
+                <div class="mb-4 min-w-[50%] md:min-w-[25%]">
+                    <span class="font-bold text-xxxl">2.2K+</span>
+                    <span class="text-regular text-gray block">Happy Clients</span>
+                </div>
+                <div class="mb-4 min-w-[50%] md:min-w-[25%]">
+                    <span class="font-bold text-xxxl">94%</span>
+                    <span class="text-regular text-gray block">Active Engagement</span>
+                </div>
+                <div class="mb-4 min-w-[50%] md:min-w-[25%]">
+                    <span class="font-bold text-xxxl">58+</span>
+                    <span class="text-regular text-gray block">Trusted Partners</span>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <?php
+<?php
 get_footer();
